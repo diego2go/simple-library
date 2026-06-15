140b8de76ce416320309e4f7113271aafb180bce
diego2go <67561210+diego2go@users.noreply.github.com>
2026-06-15 09:20:41 -0600

    Add initial displayBook functionality

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const myLibrary = [
     {
         title: 'Harry Potter',
-        author: 'J.K Rowling'
+        author: 'J.K Rowling',
+        pages: 567,
+        read: true
     },
     {
-        title: 'Why',
-        author: 'Simon Sinek'
+        title: 'Start with Why',
+        author: 'Simon Sinek',
+        pages: 400,
+        read: false
     },
     {
         title: 'Atomic Habits',
-        author: 'James Clear'
+        author: 'James Clear',
+        pages: 580,
+        read: false
     }
 ];
 
@@ -42,6 +48,34 @@ function addToLibrary(bookTitle, author, pages, readStatus) {
     myLibrary.push(book);
 }
 
+const cardContainer = document.querySelector('.card-container')
 function displayBooks() {
-    myLibrary.forEach((book) => console.log(book))
+    myLibrary.forEach((book) => {
+        
+        const bookContainer = document.createElement('div');
+        bookContainer.className = 'card';
+        
+        const bookTitle = document.createElement('h2');
+        bookTitle.className = 'bookTitle';
+        bookTitle.textContent = book.title;
+
+        const bookDetails = document.createElement('ul');
+        bookDetails.className = 'bookDetails';
+
+        const bookAuthor = document.createElement('li');
+        bookAuthor.className = 'author';
+        bookAuthor.textContent = 'Author: ' + book.author;
+       
+        const pages = document.createElement('li');
+        pages.className = 'pages';
+        pages.textContent = 'Pages: ' + book.pages; 
+  
+        const readStatus = document.createElement('li');
+        readStatus.className = 'readStatus';
+        readStatus.textContent = 'Read status: ' + book.read;
+
+        bookDetails.append(bookAuthor, pages, readStatus);
+        bookContainer.append(bookTitle,bookDetails);
+        cardContainer.append(bookContainer);
+    });
 }
