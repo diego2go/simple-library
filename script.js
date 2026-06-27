21432a3757d42730776863107c48363e1fde6011
diego2go <67561210+diego2go@users.noreply.github.com>
2026-06-27 09:30:22 -0600

    Set fnc filterBooks to simplify displayBoks()

--- script.js
+++ script.js
@@ -32,76 +32,12 @@ function addToLibrary(bookTitle, author, pages, readStatus) {
 
 const cardContainer = document.querySelector('.card-container')
 function displayBooks() {
-    // Get list of books already shown.
-    const shownBooks = document.querySelectorAll('[data-id]');
-    
-    if (shownBooks.length > 0) {
-        // set temporal array to select books to not display again;
-        const alreadyDisplayed = [];
-        // get books IDs already displayed and push to array
-        shownBooks.forEach((book) => {
-            // console.log('This ID is already on display:', book.dataset.id)
-            alreadyDisplayed.push(book.dataset.id);
-            // console.log('Pushed into alreadyDisplayed', alreadyDisplayed );
-        });
-       // skip IDs alreadyDisplayed from myLibrary, copy the rest to toDisplay
-       let toDisplay = [];
-
-        myLibrary.forEach((bookSaved) => {
-	        for (index of alreadyDisplayed) {
-		        if (index !== bookSaved.id) {
-				    // console.log(`This index ${index} is not equal to ${bookSaved.id}. So, 
-                    // this index must get into toDisplay array`);
-                    toDisplay.push(bookSaved);
-                    // console.log(`toDisplay contents: ${toDisplay}`)
-			    }else {
-				    // console.log(`This index ${index} is  equal to ${bookSaved.id}. Skip this one. 
-                    // Dont append to array toDisplay`);
-                    return
-			    }
-	        }
-        });
-        // display books from toDisplay
-        toDisplay.forEach((book) => {
-            const bookContainer = document.createElement('div');
-            bookContainer.className = 'card';
-        
-            const bookTitle = document.createElement('h2');
-            bookTitle.className = 'bookTitle';
-            bookTitle.textContent = book.title;
-
-            const bookDetails = document.createElement('ul');
-            bookDetails.className = 'bookDetails';
-
-            const bookAuthor = document.createElement('li');
-            bookAuthor.className = 'author';
-            bookAuthor.textContent = 'Author: ' + book.author;
-        
-            const pages = document.createElement('li');
-            pages.className = 'pages';
-            pages.textContent = 'Pages: ' + book.pages; 
-    
-            const readStatus = document.createElement('li');
-            readStatus.className = 'readStatus';
-            readStatus.textContent = 'Read status: ' + book.read;
-            
-            //add remove btn to each new book
-            const removeBtn = document.createElement('button');
-            removeBtn.className = 'button remove-book';
-            removeBtn.textContent = 'Remove';
-
-            // set up UUID to book card
-            bookContainer.setAttribute('data-id', book.id);
-            
-            bookDetails.append(bookAuthor, pages, readStatus);
-            bookContainer.append(bookTitle,bookDetails, removeBtn);
-            cardContainer.append(bookContainer);
-        })
-
-       // append those toDisplay into DOM
-    } else {
-        // console.log('shownBooks was empty, 1st book saved')
-        myLibrary.forEach((book) => {
+    // call filterBooksToDisplay, return array...
+    console.log('calling filterBooks...')
+    let libraryToDisplay = filterBooksToDisplay();
+    console.log('about to display:', libraryToDisplay);
+   
+    libraryToDisplay.forEach((book) => {
        
         const bookContainer = document.createElement('div');
         bookContainer.className = 'card';
@@ -136,10 +72,45 @@ function displayBooks() {
         bookContainer.append(bookTitle,bookDetails, removeBtn);
         cardContainer.append(bookContainer);
     });
-    } 
-    // may setup functions to append and to create elements separately...
 }
 
+// get books already shown, and return array of books to display
+function filterBooksToDisplay() {
+    //already shown:
+    console.log('filtering books to display')
+    //get ids shown
+    const shownBooks = document.querySelectorAll('[data-id]');
+    console.log('Ids on page:', shownBooks);
+   
+    if (shownBooks.length > 0) {
+        
+        let toDisplay = [];
+        let displayedID = [];
+        shownBooks.forEach((book) => {
+            displayedID.push(book.dataset.id);
+        });
+
+        myLibrary.forEach((book) => {
+            for (id of displayedID) {
+                if (id !== book.id) {
+				    // console.log(`This ID ${id} is not equal to ${book.id}. So,
+                    // this book must get into toDisplay array`); 
+                    toDisplay.push(book);
+                    console.log('todisplay', toDisplay)
+                } else {
+                    return
+                }
+            }
+        });
+
+        console.log('returning', toDisplay);
+        return toDisplay;
+
+    // otherwise return myLibrary
+    }else {
+        return myLibrary
+    }
+}
 // set up event listener on submit and get data
 const saveBtn = document.querySelector('#save-button')
 const form = document.querySelector('.form-inputs')
@@ -165,7 +136,10 @@ saveBtn.addEventListener('click', (event) => {
 }
     )
 
+
+
 // TO DO 
+//Still getting duplicates. Check if block...
 // 5. Add a button on each book’s display to remove the book from the library.
 //  You will need to associate your DOM elements with the actual book objects in some way.
 //  One easy solution is giving them a data-attribute that corresponds to the 
